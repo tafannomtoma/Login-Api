dfb391bf5e0f18b707a6e5f96abe7f0ae00b6c97
tafannomtoma <tafannomtoma@gmail.com>
2022-01-20 21:33:06 +0600

    sidenavbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../Login/Login';
+import SideDrawer from '../SideDrawer/SideDrawer';
+
+const Home = () => {
+    return (
+        <div>
+            {/* <Login></Login> */}
+            <SideDrawer></SideDrawer>
+        </div>
+    );
+};
+
+export default Home;
